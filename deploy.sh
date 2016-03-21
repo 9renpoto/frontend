c8189c3361a66a18f54ec55b99a068d39422e01f
Keisuke Kan <9renpoto@gmail.com>
2016-03-21 18:58:13 +0900

    add deploy task

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+git init
+git add -A
+msg="[ci skip] publish / `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+git push -f git@github.com:9renpoto/slides.git master:gh-pages
