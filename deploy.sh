ee0333d2b4daeb925f326f56429c9868d2868dc8
Keisuke Kan <9renpoto@gmail.com>
2016-08-20 19:06:58 +0900

    fix(deploy) fix deploy task

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,20 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-npm run clean || exit 1
+npm run clean && mkdir dist && cd dist || exit 1
 
-mkdir dist && cd dist
 git init
 git remote add origin git@github.com:9renpoto/slides.git
-git checkout -b gh-pages origin/gh-pages
+git fetch --prune
+git checkout -b gh-pages origin/ph-pages
 cd ..
 
-npm run build || exit 1
+npm run build && cd dist|| exit 1
 
 msg="[ci skip] publish / `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 
-git add -A
 git commit -m "$msg"
 git push origin gh-pages
