a9156d40df59bbd6406a654fdf981960dd451ce3
Keisuke Kan <9renpoto@gmail.com>
2017-02-10 22:45:17 +0900

    refactor(package) remove unused

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ module.exports = {
     'standard'
   ],
   env: {
-    'browser': true,
-    'mocha': true
+    'browser': true
   },
   rules: {
-    'consistent-return': 2,
     'no-else-return': 2,
     'no-var': 2,
     'prefer-const': 2,
