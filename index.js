01d49809c993f1fb095199156c1df6add90ac884
Keisuke Kan <9renpoto@gmail.com>
2017-05-06 23:22:54 +0900

    feat(package) prettier activate

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 module.exports = {
   extends: 'standard',
-  plugins: [
-    'standard',
-    'eslint-comments'
-  ],
+  plugins: ['standard', 'eslint-comments', 'prettier'],
   env: {
-    'browser': true
+    browser: true
   },
   rules: {
+    'prettier/prettier': ['warn', { trailingComma: 'none', singleQuote: true }],
     'eslint-comments/disable-enable-pair': 2,
     'eslint-comments/no-duplicate-disable': 2,
     'eslint-comments/no-unlimited-disable': 2,
