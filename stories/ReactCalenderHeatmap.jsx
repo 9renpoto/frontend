ff47a3739905b3d513c688fbbd06924ec92ff91d
Keisuke Kan <9renpoto@gmail.com>
2017-08-26 11:54:40 +0900

    fix(src) revert used css (#198)
    * fix(src) revert used css
    
    * chore(lib) build

--- stories/ReactCalenderHeatmap.jsx
+++ stories/ReactCalenderHeatmap.jsx
@@ -1,18 +1,35 @@
 /* @flow */
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { random, date } from 'faker'
 
 import CalendarHeatmap from 'react-calendar-heatmap'
 import '../src/components/ReactCalenderHeatmap.css'
 
+const DATE = new Date()
+
+function createRows (max: number = random.number({ max: 365 })) {
+  const rows = []
+  for (let i = 0; i < max; i++) {
+    const d = date.past()
+    rows.push({
+      date: `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`,
+      count: random.number({ max: 4 })
+    })
+  }
+  return rows
+}
+
 storiesOf('ReactCalenderHeatmap', module).add('default', () =>
   <CalendarHeatmap
-    endDate={new Date('2016-04-01')}
-    numDays={200}
-    values={[
-      { date: '2016-01-01', count: 1 },
-      { date: '2016-01-22', count: 1 },
-      { date: '2016-01-30', count: 1 }
-    ]}
+    endDate={DATE}
+    numDays={365}
+    values={createRows()}
+    classForValue={(value: any) => {
+      if (!value) {
+        value = { count: 0 }
+      }
+      return `color-github-${value.count}`
+    }}
   />
 )
