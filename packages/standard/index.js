f3a97b8c31f1985f8585d8a37cb1cae81058184a
Keisuke Kan <9renpoto@gmail.com>
2018-01-27 00:55:42 +0900

    chore(packages/standard) use prefer-destructuring (#51)

--- packages/standard/index.js
+++ packages/standard/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: ['standard'],
   rules: {
-    'sort-imports': 2,
     'no-else-return': 2,
     'no-var': 2,
     'prefer-const': 2,
+    'prefer-destructuring': 2,
     'valid-jsdoc': 2
   }
 }
