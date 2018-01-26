24f707872b46a9b864b59d91a19f66b3115fc47b
Keisuke Kan <9renpoto@gmail.com>
2018-01-27 00:50:48 +0900

    chore(packages/standard) use prefer-destructuring

--- packages/standard/test/test.js
+++ packages/standard/test/test.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const d = { hello: 'ok' }
+
 /**
  * print
  * @param  {any} str print any object
  * @return {void}
  */
-function print (str = 'ok') {
+function print ({ str } = d) {
   if ((str === 'ok' && true) || !0) {
     console.log(str)
   }
