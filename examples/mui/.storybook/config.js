9d047483771cd5f89f67ae7a3f2cbfe3d46a5510
Keisuke Kan <9renpoto@gmail.com>
2019-01-09 10:37:30 +0900

    feat(examples) try material-ui

--- examples/mui/.storybook/config.js
+++ examples/mui/.storybook/config.js
@@ -1,6 +1,7 @@
 const { configure } = require('@storybook/react')
-const req = require.context('../components/', true, /story.tsx?$/)
 
-configure(() => {
-  req.keys().forEach(filename => req(filename))
-}, module)
+function loadStories() {
+  require('../src/story.tsx')
+}
+
+configure(loadStories, module)
--- examples/mui/__tests__/Storyshots.test.ts
+++ examples/mui/__tests__/Storyshots.test.ts
@@ -1,7 +1,2 @@
 import initStoryshots from "@storybook/addon-storyshots";
-import faker from "faker";
-
-faker.seed(123);
-initStoryshots({
-  storyKindRegex: /^((?!.*?DontTest).)*$/
-});
+initStoryshots({});
