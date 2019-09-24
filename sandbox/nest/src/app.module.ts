dc8d319a8b2cf31ecff5bd86682cadd7272aaa41
Keisuke Kan <9renpoto@gmail.com>
2019-09-24 11:03:26 +0900

    try graphql

--- sandbox/nest/src/app.module.ts
+++ sandbox/nest/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
