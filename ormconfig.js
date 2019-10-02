78101c10eb5af728fe2affb5cd97147e7684dbb5
Keisuke Kan <9renpoto@gmail.com>
2019-10-02 13:54:16 +0900

    Added typeorm

---
+++ ormconfig.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const TypeOrmNamingStrategy = require('./sandbox/nest/src/TypeOrmNamingStrategy')
+
+const rootDir = ``
+const host = process.env.DATABASE_HOST || `db`
+
+module.exports = {
+  type: 'postgres',
+  host,
+  port: 5432,
+  username: 'postgres',
+  password: 'password',
+  database: 'thinking_face',
+  charset: 'utf8mb4',
+  entities: [path.join(rootDir, 'src/**/*.entity{.ts,.js}')],
+  synchronize: false,
+  migrations: [path.join(rootDir, 'db/migrations/**/*.ts')],
+  subscribers: [path.join(rootDir, 'db/subscribers/**/*.ts')],
+  cli: {
+    entitiesDir: [path.join(rootDir, '**/')],
+    migrationsDir: path.join(rootDir, 'db/migrations'),
+    subscribersDir: path.join(rootDir, 'db/subscribers'),
+  },
+  namingStrategy: new TypeOrmNamingStrategy(),
+  logging: 'all',
+}
