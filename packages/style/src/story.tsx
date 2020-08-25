41316692e6de99d6152670129acd14955ac7f1eb
Keisuke Kan <9renpoto@gmail.com>
2020-08-25 09:58:41 +0900

    chore: goodbye faker

--- packages/style/src/story.tsx
+++ packages/style/src/story.tsx
@@ -1,9 +1,8 @@
-import { random } from 'faker'
 import Button from 'preact-material-components/Button'
 import 'preact-material-components/Button/style.css'
 
 export default { title: 'material' }
 
 export function Buttons() {
-  return <Button>{random.words()}</Button>
+  return <Button>{hello}</Button>
 }
